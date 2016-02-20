6d0850ba1655d57479c082323b058d1730d17dcd
capasitore <aronsson@gmail.com>
2016-02-20 15:46:38 +0100

    temperature

---
+++ temp/temp.ino
@@ -0,0 +1,17 @@
+
+float tempC;
+int tempPin = 0;
+
+void setup(){
+  Serial.begin(9600);  
+}
+
+void loop() {
+  tempC = analogRead(tempPin);
+  tempC = (5.0 * tempC * 100.0)/1024.0;
+  Serial.print((byte)tempC);
+  Serial.println(" C");
+  delay(1000);
+  
+}
+
