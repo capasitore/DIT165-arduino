a0c79f4c124b70c8b6674d45c9cf5b061650af4e
capasitore <aronsson@gmail.com>
2016-02-21 00:10:34 +0100

    display now lights up

--- temp/temp.ino
+++ temp/temp.ino
@@ -1,17 +1,25 @@
 
+#define  ON  1
+#define  OFF  0
+
+
+
 float tempC;
 int tempPin = 0;
 
 void setup(){
-  Serial.begin(9600);  
+    Serial.begin(9600);  
 }
 
 void loop() {
-  tempC = analogRead(tempPin);
-  tempC = (5.0 * tempC * 100.0)/1024.0;
-  Serial.print((byte)tempC);
-  Serial.println(" C");
-  delay(1000);
+    //lights up segment 2 
+    pinMode(2,ON);
+    //reads temperature and prints to the serial monitor
+    tempC = analogRead(tempPin);
+    tempC = (5.0 * tempC * 100.0)/1024.0;
+    Serial.print((byte)tempC);
+    Serial.println(" C");
+    delay(1000);
   
 }
 
