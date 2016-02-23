119c8b73a5c7b1da27d07b450eb73c5b052151e7
capasitore <aronsson@gmail.com>
2016-02-23 18:41:50 +0100

    update working display temp

--- temp/temp.ino
+++ temp/temp.ino
@@ -1,29 +1,212 @@
 
-#define  ON  1
-#define  OFF  0
 
 
-
-float tempC;
 int tempPin = 0;
+int lastPoll;
+int tempC;
+int left;
+int right;
 
-void setup(){
-    Serial.begin(9600);  
+
+void setup() {
+  pinMode(1, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  Serial.begin (9600);
+  int lastPoll = millis();
+
+
+
 }
 
 void loop() {
-    //lights up segment 2 
-    //pinMode(2,ON);
-    // ex to display Number 1 light up segment 3 and 4
-    pinMode(3,ON); 
-    pinMode(4,ON); 
+  if((millis() - lastPoll) > 2000){
+    tempC = getTemp();
+    right = tempC % 10;
+    left = tempC / 10;
+    lastPoll = millis(); 
+  }
+  show(left, right);
 
-    //reads temperature and prints to the serial monitor
-    tempC = analogRead(tempPin);
-    tempC = (5.0 * tempC * 100.0)/1024.0;
-    Serial.print((byte)tempC);
-    Serial.println(" C");
-    delay(1000);
+}
+
+
+void show(int left, int right){
+  if(left == 0){
+    showRight();
+    getNumber(right);
+  } else {
+      showRight();
+      getNumber(right);
+      delay(10);
+      showLeft();
+      getNumber(left);
+      delay(10);
+  }
+}
+
+void showLeft(){
+  digitalWrite(5, LOW);
+  digitalWrite(10, HIGH);
+}
+
+void showRight(){
+  digitalWrite(5, HIGH);
+  digitalWrite(10, LOW);
+
+}
+
+void getZero(){
+  digitalWrite(1, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+  
+void getOne(){
+  digitalWrite(1, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+void getTwo(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}
+
+void getThree(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+void getFour(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+void getFive(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);  
+}
+
+void getSix(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
   
 }
+
+void getSeven(){
+  digitalWrite(1, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+
+void getEight(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);  
+}
+
+void getNine(){
+  digitalWrite(1, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);  
+}
+
+
+void getNumber(int num) {
+  switch (num) {
+    case 0:
+      getZero();
+      break; 
+    case 1:
+      getOne();
+      break; 
+    case 2:
+      getTwo();
+      break; 
+    case 3:
+      getThree();
+      break; 
+    case 4:
+      getFour();
+      break; 
+    case 5:
+      getFive();
+      break; 
+    case 6:
+      getSix();
+      break; 
+    case 7:
+      getSeven();
+      break; 
+    case 8:
+      getEight();
+      break; 
+    case 9:
+      getNine();
+      break; 
+  }
+}
+
+
+int getTemp(){
+  int tempC = analogRead(tempPin);
+  tempC = (5.0 * tempC * 100.0)/1024.0;
+  Serial.print((byte)tempC);
+  Serial.println(" C");
+  return tempC;
+}
+  
 
